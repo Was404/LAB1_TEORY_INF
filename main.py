364a7d35475c1dba9f3f93a6ce37e8fe0df8fdd0
Was403 <vladimirovichdanil@bk.ru>
2023-03-09 12:13:37 +0300

    switch columns in matrix def

--- main.py
+++ main.py
@@ -13,20 +13,55 @@ def create_matrx():
     q = 2**rows
     print("---------------------------------\nk:", rows, "\nn:", culmns, "\nCкорость кода:", speed,"\nМощность алфавита:", q ,"\n---------------------------------\n")
     
-    if m[0,0] == 1 and m[1, 1] == 1 and m[2, 2] == 1:
+    if m[0,0] == 1 and m[1, 1] == 1 and m[2, 2] == 1 and m[1,0] == 0 and m[2,0] == 0 and m[0,1] == 0 and m[0,2] == 0 and m[1,2] == 0 and m[2, 1] == 0:
         p = np.array([[m[0,3], m[0,4], m[0,5], m[0,6]], [m[1,3], m[1,4], m[0,5], m[0,6]], [m[2,3], m[2,4], m[0,5], m[2,6]]], dtype=int)
-        
+        ig = np.array([[m[0,0], m[0,1],m[0,2]], [m[1,0], m[1,1], m[1,2]], [m[2,0], m[2,1], m[2,2]]])
+    elif m[0,0] == 0 or m[0,1] == 1 or m[0,2] == 1:
+            for i in range(m):
+                 if m[0,i] == 1 and m[1,i] == 0 and m[2,i] == 0:
+                      nos = m[0,0]
+                      m[0,0] = m[0,i]
+                      m[0,i] = nos #здесь замена
+                      m[1,i] = m[1,0]
+                      m[2,i] = m[2,0]
+            p = np.array([[m[0,3], m[0,4], m[0,5], m[0,6]], [m[1,3], m[1,4], m[0,5], m[0,6]], [m[2,3], m[2,4], m[0,5], m[2,6]]], dtype=int)
+            ig = np.array([[m[0,0], m[0,1],m[0,2]], [m[1,0], m[1,1], m[1,2]], [m[2,0], m[2,1], m[2,2]]])
+    elif m[1,1] == 0 or m[0,1] == 1 or m[2,1] == 1:
+            for i in range(m):
+                 if m[1,i] == 1 and m[0,i] == 0 and m[2,i] == 0:
+                      nos = m[1,1]
+                      m[1,1] = m[1,i]
+                      m[1,i] = nos #здесь замена
+                      m[0,i] = m[0,1]
+                      m[2,i] = m[2,1]
+            p = np.array([[m[0,3], m[0,4], m[0,5], m[0,6]], [m[1,3], m[1,4], m[0,5], m[0,6]], [m[2,3], m[2,4], m[0,5], m[2,6]]], dtype=int)
+            ig = np.array([[m[0,0], m[0,1],m[0,2]], [m[1,0], m[1,1], m[1,2]], [m[2,0], m[2,1], m[2,2]]])          
+    elif m[2,2] == 0 or m[2,0] == 1 or m[2,1] == 1:
+            for i in range(m):
+                 if m[2,i] == 1 and m[0,i] == 0 and m[1,i] == 0:
+                      nos = m[2,2]
+                      m[2,2] = m[2,i]
+                      m[2,i] = nos #здесь замена
+                      m[0,i] = m[0,2]
+                      m[1,i] = m[1,2]
+            p = np.array([[m[0,3], m[0,4], m[0,5], m[0,6]], [m[1,3], m[1,4], m[0,5], m[0,6]], [m[2,3], m[2,4], m[0,5], m[2,6]]], dtype=int)
+            ig = np.array([[m[0,0], m[0,1],m[0,2]], [m[1,0], m[1,1], m[1,2]], [m[2,0], m[2,1], m[2,2]]])                                          
     p_trancp = p.transpose()
     nk = culmns - rows
     I = np.eye(nk, nk, dtype=int)  #это создание единичн матрицы
+    
+    g_sys = np.hstack((ig, p))
+    print("g_sys:\n", g_sys)
     h_sys = np.hstack((p_trancp, I))
-            #print("Транспонированная матрица:\n\n", m_trancp)
-    #inf_words = np.arange(0, 1)
     print("Систематический вид:\n", h_sys)
 
     # inf word = K log2N
-    inf_words = math.log(q) * culmns
-    print(inf_words)
+    inf_words = np.array([[0,0,0], [0,0,1], [0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0], [1,1,1]])
+    print("Информационные слова:\n", inf_words)
+    for i in range(inf_words):
+         cod_word_c = []
+         cod_word_c.append(inf_words[i] * g_sys)
+    print("Кодовые слова:\n", cod_word_c)     
     #except Exception:
     #        print("Error")
     
